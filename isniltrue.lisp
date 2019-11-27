cee18dd2453000f97c0aa8c14175227d97817670
Ryu Yamada <yamadaryutokai@yahoo.co.jp>
2019-11-27 23:20:57 +0900

    about 4 state of nil

---
+++ isniltrue.lisp
@@ -0,0 +1,16 @@
+(if '()
+    'i-am-true
+    'i-am-false)
+(if '(1)
+    'i-am-true
+    'i-am-false)
+(defun my-length (list)
+  (if list
+      (1+ (my-length (cdr list)))
+      0))
+(my-length '(list with four symbols))
+
+(eq '() nil)
+(eq '() 'nil)
+(eq '() ())
+
